e97339b79a70c7e81c5835eec7ed9973bc8ce63b
Aditya Sen <adityasen120@gmail.com>
2023-03-25 21:56:43 -0400

    add card design

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,22 @@
 import Image from "next/image";
+import styles from "./card.module.css"
 
 function Card() {
     return (
-        <button className="btn" type="button">
-            <div className="card">
-                <div className="card-body text-center">
+        <button className={`btn p-0 ${styles.card_custom}`} type="button">
+            <div className="card card-custom">
+                <div className="card-body text-center pb-1">
                     <Image
                         src={"/card-imgs/peace-sign.png"}
                         alt="card-img"
-                        width={200}
-                        height={200}
+                        width={75}
+                        height={75}
                     />
-                    <p>Peace Sign</p>
+                    <p className="mt-3">Peace Sign</p>
                 </div>
             </div>
         </button>
-    )
+    );
 }
 
 export default Card;
